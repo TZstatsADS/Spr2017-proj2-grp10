1d22d413ce6e305ba1cb40e5ed5d8cbb89f333ca
jt2929 <jt2929@columbia.edu>
2017-02-17 00:06:42 -0500

    Final data after crime and happiness

---
+++ doc/merge_crime_happiness.R
@@ -0,0 +1,14 @@
+#Building on top of DataWithRank
+#Import crime and happiness datasets
+crime<-read.csv("CrimeData_final.csv")
+happy<-read.csv("Happinessdata.csv")
+datarank<-read.csv("DataWithRank.csv")
+
+#Merge happy and crime data
+crimehappy<-merge(crime, happy, by="State")
+colnames(crimehappy)[3]<-"State"
+crimehappy<-crimehappy[,-1]
+
+#Merge happycrime data into full DataWithRank dataset
+FinalData<-merge(DataWithRank, crimehappy, by="State") %>% arrange(Rank) 
+FinalData<-FinalData[, -2] %>% write_csv("../data/finaldata.csv")
