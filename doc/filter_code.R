126e5237d4f5cda0e6606046fb1f590ea10ef58c
jt2929 <jt2929@columbia.edu>
2017-02-22 11:49:32 -0500

    updated filter code on edited UI

--- doc/filter_code.R
+++ doc/filter_code.R
@@ -1,29 +1,43 @@
-shinyServer(function(input,output){
+shinyServer(function(input, output){
   #read data
-  schdata<- read.csv("final3data.csv")
+  schdata<- read.csv("~/Desktop/Spr2017-proj2-proj2_team10/data/final3data.csv")
   #subset data depending on user input in Shiny app
-  schinput<-reactive({
-    {if (input$major == "NA") {
-      v1<-schdata
-    }
-    else {
-      selectmaj<-paste("Major", "_", input$major, sep="")
-      v1<- schdata %>% filter_(paste(selectmaj, "==", 1))
-    }}
-      
-    {if (input$city == "NA") {
-      v2<- v1} 
-    else {
-      v2<- filter(v1, Citytype %in% input$city)
-    }} 
-    
-    {if (input$schooltype == "NA") {
-      v3<-v2
-    }  
-    else {
-      v3<- filter(v2, Ownership %in% input$schooltype)
-    }}
   
+  major<-reactive({
+    major<-input$major
   })
   
-})
+  st<-reactive({
+    st<-input$schtype
+  })
+  
+  ct<-reactive({
+    ct<-input$citytype
+  })
+  
+  v1<-reactive({
+    if (major() == "None") {
+      v1<-schdata
+    } 
+    else {
+      selectmaj<-paste("Major", "_", major(), sep="")
+      v1<- schdata %>% filter_(paste(selectmaj, "==", 1))}}) 
+  v2<- reactive({
+    if (st() == "None") {
+      v2<-v1() 
+    }  
+    else {
+      v2<- filter(v1(), Ownership==st())
+    }}) 
+  
+  v3<- reactive({
+    if (ct() == "None") {
+      v3<- v2()} 
+    else {
+      v3<- filter(v2(), Citytype==ct()) 
+    }})   
+  
+  output$tablerank = renderDataTable({
+    final1dat<-v3()
+    final1dat[,c(3,4,5,6)]
+  },options = list(orderClasses = TRUE))}) 
