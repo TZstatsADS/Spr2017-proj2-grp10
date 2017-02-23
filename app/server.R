951c534cf54616093e85951c81b4c7ad89ae48f2
amandazhang <amandazhang@berkeley.edu>
2017-02-23 10:42:28 -0500

    move files to right places

--- app/server.R
+++ app/server.R
@@ -1,121 +1,122 @@
+packages.used=c("dplyr", "plotly", "shiny", "leaflet", "scales", 
+                "lattice", "htmltools", "maps", "data.table", 
+                "dtplyr")
+
+# check packages that need to be installed.
+packages.needed=setdiff(packages.used, 
+                        intersect(installed.packages()[,1], 
+                                  packages.used))
+# install additional packages
+if(length(packages.needed)>0){
+  install.packages(packages.needed, dependencies = TRUE)
+}
+
+
 library(shiny)
-library(choroplethr)
-library(choroplethrZip)
+library(leaflet)
+library(scales)
+library(lattice)
 library(dplyr)
+library(htmltools)
+library(maps)
+library(plotly)
+library(data.table)
+library(dtplyr)
 
-## Define Manhattan's neighborhood
-man.nbhd=c("all neighborhoods", "Central Harlem", 
-           "Chelsea and Clinton",
-           "East Harlem", 
-           "Gramercy Park and Murray Hill",
-           "Greenwich Village and Soho", 
-           "Lower Manhattan",
-           "Lower East Side", 
-           "Upper East Side", 
-           "Upper West Side",
-           "Inwood and Washington Heights")
-zip.nbhd=as.list(1:length(man.nbhd))
-zip.nbhd[[1]]=as.character(c(10026, 10027, 10030, 10037, 10039))
-zip.nbhd[[2]]=as.character(c(10001, 10011, 10018, 10019, 10020))
-zip.nbhd[[3]]=as.character(c(10036, 10029, 10035))
-zip.nbhd[[4]]=as.character(c(10010, 10016, 10017, 10022))
-zip.nbhd[[5]]=as.character(c(10012, 10013, 10014))
-zip.nbhd[[6]]=as.character(c(10004, 10005, 10006, 10007, 10038, 10280))
-zip.nbhd[[7]]=as.character(c(10002, 10003, 10009))
-zip.nbhd[[8]]=as.character(c(10021, 10028, 10044, 10065, 10075, 10128))
-zip.nbhd[[9]]=as.character(c(10023, 10024, 10025))
-zip.nbhd[[10]]=as.character(c(10031, 10032, 10033, 10034, 10040))
-
-## Load housing data
-load("../output/count.RData")
-load("../output/mh2009use.RData")
-
-# Define server logic required to draw a histogram
-shinyServer(function(input, output) {
+shinyServer(function(input, output){
+  #read data
+  schdata<- read.csv("final3data.csv")
+  #subset data depending on user input in Shiny app
   
-  ## Neighborhood name
-  output$text = renderText({"Selected:"})
-  output$text1 = renderText({
-      paste("{ ", man.nbhd[as.numeric(input$nbhd)+1], " }")
+  major<-reactive({
+    major<-input$major
   })
   
-  ## Panel 1: summary plots of time trends, 
-  ##          unit price and full price of sales. 
-  
-  output$distPlot <- renderPlot({
-    
-    ## First filter data for selected neighborhood
-    mh2009.sel=mh2009.use
-    if(input$nbhd>0){
-      mh2009.sel=mh2009.use%>%
-                  filter(region %in% zip.nbhd[[as.numeric(input$nbhd)]])
-    }
-    
-    ## Monthly counts
-    month.v=as.vector(table(mh2009.sel$sale.month))
-    
-    ## Price: unit (per sq. ft.) and full
-    type.price=data.frame(bldg.type=c("10", "13", "25", "28"))
-    type.price.sel=mh2009.sel%>%
-                group_by(bldg.type)%>%
-                summarise(
-                  price.mean=mean(sale.price, na.rm=T),
-                  price.median=median(sale.price, na.rm=T),
-                  unit.mean=mean(unit.price, na.rm=T),
-                  unit.median=median(unit.price, na.rm=T),
-                  sale.n=n()
-                )
-    type.price=left_join(type.price, type.price.sel, by="bldg.type")
-    
-    ## Making the plots
-    layout(matrix(c(1,1,1,1,2,2,3,3,2,2,3,3), 3, 4, byrow=T))
-    par(cex.axis=1.3, cex.lab=1.5, 
-        font.axis=2, font.lab=2, col.axis="dark gray", bty="n")
-    
-    ### Sales monthly counts
-    plot(1:12, month.v, xlab="Months", ylab="Total sales", 
-         type="b", pch=21, col="black", bg="red", 
-         cex=2, lwd=2, ylim=c(0, max(month.v,na.rm=T)*1.05))
-    
-    ### Price per square foot
-    plot(c(0, max(type.price[,c(4,5)], na.rm=T)), 
-         c(0,5), 
-         xlab="Price per square foot", ylab="", 
-         bty="l", type="n")
-    text(rep(0, 4), 1:4+0.5, paste(c("coops", "condos", "luxury hotels", "comm. condos"), 
-                                  type.price$sale.n, sep=": "), adj=0, cex=1.5)
-    points(type.price$unit.mean, 1:nrow(type.price), pch=16, col=2, cex=2)
-    points(type.price$unit.median, 1:nrow(type.price),  pch=16, col=4, cex=2)
-    segments(type.price$unit.mean, 1:nrow(type.price), 
-              type.price$unit.median, 1:nrow(type.price),
-             lwd=2)    
-    
-    ### full price
-    plot(c(0, max(type.price[,-1], na.rm=T)), 
-         c(0,5), 
-         xlab="Sales Price", ylab="", 
-         bty="l", type="n")
-    text(rep(0, 4), 1:4+0.5, paste(c("coops", "condos", "luxury hotels", "comm. condos"), 
-                                   type.price$sale.n, sep=": "), adj=0, cex=1.5)
-    points(type.price$price.mean, 1:nrow(type.price), pch=16, col=2, cex=2)
-    points(type.price$price.median, 1:nrow(type.price),  pch=16, col=4, cex=2)
-    segments(type.price$price.mean, 1:nrow(type.price), 
-             type.price$price.median, 1:nrow(type.price),
-             lwd=2)    
+  st<-reactive({
+    st<-input$schtype
   })
   
-  ## Panel 2: map of sales distribution
-  output$distPlot1 <- renderPlot({
-    count.df.sel=count.df
-    if(input$nbhd>0){
-      count.df.sel=count.df%>%
-        filter(region %in% zip.nbhd[[as.numeric(input$nbhd)]])
-    }
-    # make the map for selected neighhoods
-    
-    zip_choropleth(count.df.sel,
-                   title       = "2009 Manhattan housing sales",
-                   legend      = "Number of sales",
-                   county_zoom = 36061)
+  ct<-reactive({
+    ct<-input$citytype
   })
+  
+  v1<-reactive({
+    if (major() == "None") {
+      v1<-schdata
+    } 
+    else {
+      selectmaj<-paste("Major", "_", major(), sep="")
+      v1<- schdata %>% filter_(paste(selectmaj, "==", 1))}}) 
+  v2<- reactive({
+    if (st() == "None") {
+      v2<-v1() 
+    }  
+    else {
+      v2<- filter(v1(), Ownership==st())
+    }}) 
+  
+  v3<- reactive({
+    if (ct() == "None") {
+      v3<- v2()} 
+    else {
+      v3<- filter(v2(), Citytype==ct()) 
+    }})   
+  
+  output$tablerank = renderDataTable({
+    final1dat<-v3()
+    final1dat[,c(3,4,13,14,27,29)]
+  },options = list(orderClasses = TRUE))
+  
+  
+  output$tablerank2 <- renderDataTable({
+    data<-v3()
+    w1<-input$Earn
+    w2<-input$rank
+    w3<-input$AvgCost
+    w4<-input$CrimeRate
+    w5<-input$HappyRank
+    final2dat<-data %>%
+      select(Name,Earn, ADMrate,Rank,AvgCost,CrimeRate,HappyRank)%>%
+      arrange(Earn,Rank)%>%
+      mutate(Earn1=seq(1:nrow(data)))%>%
+      arrange(AvgCost,Rank)%>%
+      mutate(AvgCost1=seq(1:nrow(data)))%>%
+      arrange(CrimeRate,Rank)%>%
+      mutate(CrimeRate1=seq(1:nrow(data)))%>%
+      arrange(HappyRank,Rank)%>%
+      mutate(HappyRank1=seq(1:nrow(data))) %>%
+      mutate(new_rank=w1*Earn1+ w2*Rank+w3*AvgCost1+w4*CrimeRate1+w5*HappyRank1) %>%
+      arrange(new_rank)
+    final2dat[,c(4,1,2,3,5,7)]
+  },options = list(orderClasses = TRUE))
+  
+  
+  output$map <- renderLeaflet({
+    urls <- paste0(as.character("<b><a href='http://"), as.character(v3()$URL), "'>", as.character(v3()$Name),as.character("</a></b>"))
+    content <- paste(sep = "<br/>",
+                     urls, 
+                     paste("Rank:", as.character(v3()$Rank))
+    )
+    
+    mapStates = map("state", fill = TRUE, plot = FALSE)
+    leaflet(data = mapStates) %>% addTiles() %>%
+      addPolygons(fillColor = topo.colors(10, alpha = NULL), stroke = FALSE) %>%
+      addMarkers(v3()$Longitude, v3()$Latitude, 
+                 popup = content)
+  })
+  
+  output$bchart <- renderPlotly({
+    edu <- v3()
+    # Density curve for Earning:
+    earn <- edu %>% select(Name, Earn) %>% arrange(desc(Earn)) 
+    earn$Earn <- as.numeric(as.character(earn$Earn))
+    a <- ggplot(data = earn,aes(x=Earn)) + 
+      geom_histogram(aes(y = ..density..), alpha = 0.7, fill = "#56B4E9") +
+      geom_density(fill = "#CC79A7", alpha = 0.5) +
+      theme(panel.background = element_rect(fill = '#ffffff')) + 
+      ggtitle("Density of Earning with Histogram overlay")
+    ggplotly(a)
+    
+  }
+  )
 })
